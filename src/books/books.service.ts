70d5208f2bb17a658c8f9c85775a42051fbcb2f3
Vania Abbas <vaniaabbas2@gmail.com>
2025-09-21 00:49:33 +0500

    added comments

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -13,11 +13,22 @@ export class BooksService {
     private bookRepository: Repository<Book>,
   ) {}
 
+  /**
+   * Fetches all books
+   * @param res - Express response object
+   * @returns All books or 404 if not found
+   */
   async findAll(res: express.Response) {
     const books = await this.bookRepository.find();
     return res.status(200).json(books);
   }
 
+  /**
+   * Fetches a book by ID
+   * @param id - UUID of the book
+   * @param res - Express response object
+   * @returns A book or 404 if not found
+   */
   async findOne(id: string, res: express.Response) {
     const book = await this.bookRepository.findOne({ where: { id } });
     if (!book) {
@@ -26,18 +37,38 @@ export class BooksService {
     return res.status(200).json(book);
   }
 
+  /**
+   * Creates a book
+   * @param dto - Partial data to create a book
+   * @param res - Express response object
+   * @returns 201 - success
+   */
   async create(dto: CreateBookDto, res: express.Response) {
     const book = this.bookRepository.create(dto);
     const savedBook = await this.bookRepository.save(book);
     return res.status(201).json(savedBook);
   }
 
+  /**
+   * Updates a book by ID
+   * @param id - UUID of the book
+   * @param dto - Partial data to update
+   * @param res - Express response object
+   * @returns Updated book or 404 if not found
+   */
   async update(id: string, dto: UpdateBookDto, res: express.Response) {
     const book = await this.bookRepository.findOne({ where: { id } });
     if (!book) {
       return res.status(404).json({ message: 'Book Not Found' });
     }
   }
+
+  /**
+   * Updates a book by ID
+   * @param id - UUID of the book
+   * @param res - Express response object
+   * @returns Deletion Message or 404 if not found
+   */
   async remove(id: string, res: express.Response) {
     const book = await this.bookRepository.delete(id);
     if (book.affected === 0) {
